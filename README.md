089e3869446e63fe3907163103c8ad30e09aa2ca
Rakibul Hasan <rakibulhasan9594@gmail.com>
2026-09-13 17:23:00 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+##React ALL Code
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import profileImage from "./images/CV_image_update (1).jpg"
+
+function App() {
+  return (
+    <div>
+    <img
+      src={profileImage}
+      alt="Rakibul Hasan"
+    />
+    </div>
+  );
+}
+
+export default App;
